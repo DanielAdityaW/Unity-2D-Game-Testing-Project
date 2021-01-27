a3e8c4f1b2313d12d6964db08171d3f1de4c661f
Venomeio <77531735+Venomeio@users.noreply.github.com>
2021-01-27 23:14:54 +0700

    New Update 27 Jan 21

---
+++ Testing Project/Assets/SCRIPTS/Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public CharacterController2D kontrol;
+    private float gerakHorizontal = 0f;
+    public float kecepatanGerak = 0f;
+    private bool lompat = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gerakHorizontal = Input.GetAxisRaw("Horizontal") * kecepatanGerak;
+        if (Input.GetButtonDown("Jump")){
+            lompat = true;
+        } 
+    }
+
+    void FixedUpdate() 
+    {
+        kontrol.Move(gerakHorizontal * Time.fixedDeltaTime , false, lompat);
+        lompat = false;
+    }
+}
