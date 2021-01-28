1e6b7277beca11c61e99ad2dad5f6506ab04bcb5
Venomeio <77531735+Venomeio@users.noreply.github.com>
2021-01-28 23:20:12 +0700

    update

--- Testing Project/Assets/SCRIPTS/PlayerController.cs
+++ Testing Project/Assets/SCRIPTS/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float kecepatan, gayaLompat, checkRadius;
-    private float moveInput;
+    public float kecepatan, gayaLompat, checkRadius, kecepatanDorong, waktuMulaiDorong;
+    private float moveInput,waktuDorong;
 
     private Rigidbody2D rb2d;
 
@@ -14,17 +14,24 @@ public class PlayerController : MonoBehaviour
     public Transform groundCheck;
     public LayerMask whatIsGround;
 
-    private int lompat;
+    private int lompat, arahDorong;
     public int nilaiLompat;
 
     private void Start()
     {
         lompat = nilaiLompat;
         rb2d = GetComponent<Rigidbody2D>();
+        waktuDorong = waktuMulaiDorong;
     }
 
-    private void Update()
+    void Update()
     {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        {
+            Debug.Log(Input.GetAxisRaw("Horizontal"));
+        }
+        rb2d.velocity = new Vector2 (moveInput * kecepatan, rb2d.velocity.y);
+
         if(isGround == true){
             lompat = nilaiLompat;
         }
@@ -35,14 +42,37 @@ public class PlayerController : MonoBehaviour
         }else if (Input.GetKeyDown(KeyCode.Space) && lompat == 0 && isGround == true){
             rb2d.velocity = Vector2.up * gayaLompat;
         }
+
+        if (arahDorong == 0){
+            if(Input.GetKeyDown(KeyCode.F)){
+                if (moveInput > 0){
+                    Debug.Log("Kanan");
+                    arahDorong = 1;
+                } else if (moveInput < 0){
+                    Debug.Log("Kiri");
+                    arahDorong = 2;
+                }
+            }
+        } else {
+            if(waktuDorong <= 0){
+                arahDorong = 0;
+                waktuDorong = waktuMulaiDorong;
+                rb2d.velocity = Vector2.zero;
+            } else {
+                waktuDorong -= Time.deltaTime;
+                if (arahDorong == 1){
+                    rb2d.velocity = Vector2.right * kecepatanDorong;
+                } else if (arahDorong == 2){
+                    rb2d.velocity = Vector2.left * kecepatanDorong;
+                }
+            }
+        } 
+        
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         isGround = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-
-        moveInput = Input.GetAxisRaw("Horizontal");
-        rb2d.velocity = new Vector2 (moveInput * kecepatan, rb2d.velocity.y);
 
         if (facingRight == false && moveInput > 0){
             Flip();
